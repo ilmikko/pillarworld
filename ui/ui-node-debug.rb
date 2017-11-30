51a62be4b67b6a2d2d4b32496b8ace368011b994
ilmikko <mikko@5x.fi>
2017-11-30 19:42:17 +0000

    Node debug setting is now appreciated

--- ui/ui-node-debug.rb
+++ ui/ui-node-debug.rb
@@ -1,15 +1,21 @@
 class UINode
 	@@debugcolors=(41..47).to_a+(100..107).to_a;
-	@@debug=false;
+	@@debug=true;
 	def redraw()
 		col=@@debugcolors.sample;
-		if @write.nil?
-			text=self.class;
-			# Generate a thing to write
-			canvas.write(*@xy,"<#{text}",color:"\e[#{col}m");
-			canvas.write(*[@xy[0]+@wh[0]-1-"#{text}".length,@xy[1]+@wh[1]-1],"#{text}>",color:"\e[#{col}m");
-		elsif !canvas.nil?
-			canvas.write(*@xy,@write,color:"\e[#{col}m");
+		if @@debug
+			if @write.nil?
+				text=self.class;
+				# Generate a thing to write
+				canvas.write(*@xy,"<#{text}",color:"\e[#{col}m");
+				canvas.write(*[@xy[0]+@wh[0]-1-"#{text}".length,@xy[1]+@wh[1]-1],"#{text}>",color:"\e[#{col}m");
+			elsif !canvas.nil?
+				canvas.write(*@xy,@write,color:"\e[#{col}m");
+			end
+		else
+			if !@write.nil? && !@canvas.nil?
+				@canvas.write(*@xy,@write,color:@color);
+			end
 		end
 	end
 end
