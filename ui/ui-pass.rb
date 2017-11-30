a96e1bfaea2867a89f83b1cb2b680f9a91134d7c
ilmikko <mikko@5x.fi>
2017-11-30 18:58:49 +0000

    UIPass now alerts if there are too many children

--- ui/ui-pass.rb
+++ ui/ui-pass.rb
@@ -4,5 +4,12 @@
 # Inherited and used in modules like UIBorder.
 #
 class UIPass < UIArray
-	# Pass doesn't do much on its own. It just passes everything to its child like it wasn't there at all.
+	# Pass prevents more than 1 children to exist simultaneously.
+	# This is to prevent headaches with overlapping elements (Pass doesn't know how to position them!)
+	# You should have probably used a Flex instead.
+	def append(*items)
+		multierror = "Error: #{self.class} cannot contain multiple elements (tried to have #{items.length+@children.length}).";
+		raise multierror if items.length+@children.length>1;
+		super(*items);
+	end
 end
