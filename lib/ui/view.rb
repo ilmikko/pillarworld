76293e7bafb01eacb0d4460a35311f45ca58e07f
ilmikko <mikko@5x.fi>
2017-12-24 22:34:01 +0000

    View now tracks the reads/writes, and has an option to update automatically (fps thread)

--- lib/ui/view.rb
+++ lib/ui/view.rb
@@ -8,6 +8,8 @@
 #
 
 class UI::View < UI::Node
+	attr_reader :fps
+
 	def scene=(v)
 		@scene=v;
 		redraw; # Always redraw when scene changes
@@ -15,18 +17,74 @@ class UI::View < UI::Node
 
 	def put(x,y,char)
 		w,h=@wh;
+		x=x.round;
+		y=y.round;
 		return if x<0 or y<0 or x>=w or y>=h; # Prevent writing outside of the view
+		
+		# Return if we already have this in the cache (trying to rewrite a cell)
+		return if @clearcache["#{x},#{y}"]==char;
+		# Store in cache
+		@clearcache["#{x},#{y}"]=char;
 
 		sx,sy=@xy;
 		@@screen.put(sx+x,sy+y,char);
 	end
 
-	def redraw
-		#$console.log("Redrawing #{self} with scene #{@scene}");
-		@scene.() if @scene;
+	def clear
+		# TODO: clearing in different situations
+		sx,sy=@xy;
+		@clearcache.dup.each{|k,v|
+			$console.log("#{k}->#{v}");
+			x,y=k.split(",");
+			@@screen.erase(sx+x.to_i,sy+y.to_i);
+		}
+		reset;
 	end
 
-	def initialize(scene=nil,**_)
+	def reset
+		# Reset the clearing cache
+		@clearcache={};
+	end
+
+	def change
+		# When our attributes change, the screen has redrawn and we need to reset our clearing cache.
+		reset;
+	end
+
+	def redraw(timedelta=0)
+		#$console.log("Redrawing #{self} with scene #{@scene}");
+		if @scene
+			if @scene.arity>0
+				# Pass the time delta if the scene accepts arity
+				@scene.(timedelta);
+			else
+				# Else just call with no arguments
+				# TODO: In this case, we're having timestamps in the fps thread which are utterly useless.
+				# Please FIXME.
+				@scene.();
+			end
+		end
+	end
+
+	def initialize(scene=nil,fps:-1,**_)
+		@fps=fps;
+
+		@clearcache={};
+		
+		if fps>0
+			$console.log("View #{self} creates an fps thread with fps=#{@fps}");
+			Thread.new{
+				stamp=Time.now;
+				loop{
+					clear;
+					redraw(Time.now-stamp);
+					slp=1/@fps.to_f;
+					stamp=Time.now;
+					sleep(slp);
+				}
+			}
+		end
+
 		super(**_);
 		self.scene=scene if !scene.nil?;
 	end
