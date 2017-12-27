4e614a728de0e684ed5f3651bb9b46b67f6976d8
ilmikko <mikko@5x.fi>
2017-12-27 22:05:04 +0000

    Evented is now a module

---
+++ lib/evented.rb
@@ -0,0 +1,12 @@
+module Evented
+	def fire(name)
+		return if @events.nil?
+		return if !@events.key?(name);
+		@events[name].each{|f| f.(); }
+	end
+	def on(name,callback)
+		@events={} if @events.nil?;
+		@events[name]=[] if @events[name].nil?;
+		@events[name].push(callback);
+	end
+end
