fa644fcc4aa726a2bea49a369e867b8f4e265f53
ilmikko <mikko@5x.fi>
2017-12-28 14:40:51 +0000

    Reordered screen/color

--- lib/screen/color.rb
+++ lib/screen/color.rb
@@ -7,11 +7,53 @@
 #	most appropriate color.
 #
 
+# We have a couple of different ways of representing colors.
+# The most obvious ones are:
+# foreground color
+# background color
+#
+# But there are also some others that modify the colors / visuals, like:
+# bold
+# faint (maybe)
+# italic (not widely supported)
+# underline
+# negative (reverse video)
+#
+
 class Screen::Color
-	module Screen::Color::Defaults
-		def convert(r,g,b)
-			''
+	@@mode=:none;
+	def self.mode;@@mode;end
+	def self.mode=(v)
+		#
+		# As we're playing with Ruby's include, a module can only be included once into the program.
+		# This means that if you were to do
+		# ```
+		# Screen::Color.mode=:optimal
+		# Screen::Color.mode=:reduced
+		# Screen::Color.mode=:optimal
+		# ```
+		# The color mode would stay in reduced as optimal has already been included here.
+		# Currently it's not an issue as the color mode should NOT be changed after the init;
+		# however, I might add a 'force' option later in the case that we do need to
+		# cycle through the color modes, for example in the settings.
+		#
+		$console.log("Setting color mode to #{v}...");
+		$console.warn("Changing the color mode multiple times is not recommended, as it may lead to unexpected behavior.") if @@mode!=:none;
+		if v==:optimal
+			include Screen::Color::Optimal
+		elsif v==:reduced
+			include Screen::Color::Reduced
+		elsif v==:bare
+			include Screen::Color::Bare
+		elsif v==:system
+			include Screen::Color::System
+		else
+			raise "Unknown color mode: #{v}";
 		end
+		@@mode=v;
+	end
+
+	module Screen::Color::Defaults
 		def initialize(r,g,b)
 			# Reduce this to an ANSI color - this way we can then check for color overlaps in screen
 			@original=[r,g,b];
@@ -68,38 +110,6 @@ class Screen::Color
 end
 
 class Screen::Color
-	@@mode=:none;
-	def self.mode;@@mode;end
-	def self.mode=(v)
-		#
-		# As we're playing with Ruby's include, a module can only be included once into the program.
-		# This means that if you were to do
-		# ```
-		# Screen::Color.mode=:optimal
-		# Screen::Color.mode=:reduced
-		# Screen::Color.mode=:optimal
-		# ```
-		# The color mode would stay in reduced as optimal has already been included here.
-		# Currently it's not an issue as the color mode should NOT be changed after the init;
-		# however, I might add a 'force' option later in the case that we do need to
-		# cycle through the color modes, for example in the settings.
-		#
-		$console.log("Setting color mode to #{v}...");
-		$console.warn("Changing the color mode multiple times is not recommended, as it may lead to unexpected behavior.") if @@mode!=:none;
-		if v==:optimal
-			include Screen::Color::Optimal
-		elsif v==:reduced
-			include Screen::Color::Reduced
-		elsif v==:bare
-			include Screen::Color::Bare
-		elsif v==:system
-			include Screen::Color::System
-		else
-			raise "Unknown color mode: #{v}";
-		end
-		@@mode=v;
-	end
-
 	@@colors={
 		black: [0,0,0],
 		red: [255,0,0],
@@ -127,4 +137,8 @@ class Screen::Color
 	def to_s
 		@colstr||''
 	end
+
+	#def initialize(r,g,b)
+	#	raise "A screen color mode was not set. Please set a color mode using Screen::Color.mode=:reduced";
+	#end
 end
