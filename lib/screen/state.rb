9ed6431fdc6502aa01851c25aa9e87d09330272c
ilmikko <mikko@5x.fi>
2018-01-22 13:25:48 +0000

    Performance improvements

--- lib/screen/state.rb
+++ lib/screen/state.rb
@@ -1,21 +1,22 @@
 class Screen::State
 	attr_accessor :color,:background,:bold,:faint,:italic,:inverted,:underline;
 
-	def use
-		str=to_s;
-		$console.log("Using #{self.class} #{self.object_id} (#{str})");
-		print(str);
-	end
+	#def use
+	#	str=to_s;
+	#	$console.log("Using #{self.class} #{self.object_id} (#{str})");
+	#	print(str);
+	#end
 
 	def to_s
 		# TODO: calculate state difference to current state
 		# For example, if we have a state RED, BOLD, ITALIC
 		# and we change to RED, BOLD, ITALIC, UNDERLINE
 		# we can only print UNDERLINE because everything else is implied.
+
 		a=@modifiers.values;
 		a << @color if @color;
 		a << @background if @background;
-		str="\e[;#{a.join(';')}m";
+		str="\e[;"<< a.join(';') << 'm';
 		return str;
 	end
 
@@ -26,17 +27,10 @@ class Screen::State
 	def initialize(color:nil,background:nil,**modifiers)
 		@color=Screen::Color[*color] if !color.nil?;
 		@background=Screen::Color[*background] if !background.nil?;
-		#@modifiers={
-		#	bold: Screen::Modifier.bold(modifiers[:bold]),
-		#	faint: Screen::Modifier.faint(modifiers[:faint]),
-		#	italic: Screen::Modifier.italic(modifiers[:italic]),
-		#	negate: Screen::Modifier.negate(modifiers[:negate]),
-		#	underline: Screen::Modifier.underline(modifiers[:underline]),
-		#	invisible: Screen::Modifier.invisible(modifiers[:invisible])
-		#};
 		
 		@modifiers=modifiers.map{|mod,bool|
 			$console.log("Modifiers: #{mod} -> #{bool}");
+			raise "Unknown modifier: #{mod}" if !Screen::Modifier.respond_to? mod;
 			[mod,Screen::Modifier.public_send(mod,bool)];
 		}.to_h;
 	end
