635a2ecea0dc4bf7376822c20f24a18b8b589654
ilmikko <mikko@5x.fi>
2018-02-16 15:52:22 +0000

    Testing suite supports subtests

--- test/suite.rb
+++ test/suite.rb
@@ -5,6 +5,69 @@
 require_relative('ui');
 
 class Suite
+	class Progress
+		class Queue
+			def to_s
+				"[" << @tests.reduce(''){|a,b|a<<b.to_s} << "]";
+			end
+
+			def failures
+				@tests.reduce(0){|a,b| (!b.is_a? Queue) ? (b.status==:failure ? a+1 : a) : (a+b.failures)};
+			end
+
+			def successes
+				@tests.reduce(0){|a,b| (!b.is_a? Queue) ? (b.status==:success ? a+1 : a) : (a+b.successes)};
+			end
+
+			def total
+				@tests.reduce(0){|a,b| (!b.is_a? Queue) ? (a+1) : (a+b.total)};
+			end
+
+			def push(*items)
+				@tests.push(*items);
+			end
+
+			attr_reader :status;
+
+			private
+
+			def initialize(tests=[])
+				@status=:nothing;
+				@tests=tests;
+			end
+		end
+
+		def failures
+			@queue.failures;
+		end
+
+		def successes
+			@queue.successes;
+		end
+
+		def total
+			@queue.total;
+		end
+
+		def to_s
+			@queue.to_s;
+		end
+
+		def queue(test)
+			@queue.push(test);
+		end
+
+		def queue_bunch(tests)
+			@queue.push(Queue.new(tests));
+		end
+
+		private
+
+		def initialize
+			@queue=Queue.new;
+		end
+	end
+
 	class Test
 		def Test.create(pr)
 			if pr.is_a? Proc
@@ -15,34 +78,80 @@ class Suite
 				raise "Cannot create a test with class #{pr.class}";
 			end
 		end
+
+		@@test_symbols={
+			nothing: '.',
+			running: ':',
+			success: '+',
+			failure: '!'
+		};
 		@@id=0;
+
 		def to_s
-			"<Test ##{@id}>"
+			@@test_symbols[@status]||'?';
 		end
-		def run
+		
+		def run_test
 			@proc.();
 		end
+
+		def run_start
+				@status=:running;
+		end
+
+		def run
+			begin
+				run_test;
+				@status=:success;
+			rescue Exception => e
+				@status=:failure;
+				raise e;
+			end
+		end
+
+		attr_reader :status;
+
+		private
+
 		def initialize(pr)
+			@status=:nothing;
 			@proc=pr;
 			@id=(@@id+=1);
 		end
 	end
 	class Test::File < Test
-		def run
+		def run_test
 			load(@proc); # treat proc as src
 		end
 	end
 
 	class Status
 		def redraw;
-			$stdout.print("\e[H\e[K\e[m#{@msg}\e[2H");
+			# Clearing
+			$stdout.print("\e[H\e[K\e[m");
+			right=@progress.to_s;
+			len = right.length;
+
+			# Very hackish colors
+			right=right.gsub(/:/,"\e[33m:\e[m");
+			right=right.gsub(/!/,"\e[31m!\e[m");
+			right=right.gsub(/\+/,"\e[32m+\e[m");
+
+			$stdout.print("\e[;#{Screen.width-len+1}H#{right}"); # Right side
+			$stdout.print("\e[;H#{@msg}\e[2H"); # Left side
 		end
 		def msg(msg)
 			$console.log("S: #{msg.gsub(/\e\[[0-9;]+m/,'')}");
 			@msg=msg;
 			redraw;
 		end
-		def final(s,f,t)
+		def final(progress)
+			# Calculate failures and successes
+			
+			f=progress.failures;
+			s=progress.successes;
+			t=progress.total;
+
 			failures="#{f} failure";
 			failures+='s' if f!=1;
 			failures="\e[31m#{failures}\e[m" if f>0;
@@ -56,39 +165,68 @@ class Suite
 
 			msg("#{tests} (#{failures}, #{successes})!");
 		end
-		def initialize(width,x:0,y:0)
-			@w=width;
+
+		private
+
+		def initialize(progress,x:0,y:0)
 			@msg='';
+			@progress=progress;
 		end
 	end
 
 	def reset;
 		print("\e[2J");
+		@status.redraw;
 	end
 
 	def queue(test)
-		@total+=1;
-		@test_queue << Suite::Test.create(test);
+		@test_queue << test;
+	end
+
+	def queue_directory(dir)
+		$console.log("Queuing #{dir}...");
+		Dir[dir].each{|file|
+			if File.directory? file
+				# Directory, queue a bit differently
+				test(*Dir[file<<'/*'].sort);
+			else
+				test(file);
+			end
+		};
 	end
 
 	def test(*tests)
-		@status.msg("Queuing #{tests.length} test(s)...");
-		tests.each{|test|
+		tests.map!{|test| Suite::Test.create(test)}
+
+		if tests.length==1
+			test=tests.first;
+
+			@progress.queue(test);
 			queue(test);
-		}
+		else
+			@status.msg("Queuing #{tests.length} test(s)...");
+
+			@progress.queue_bunch(tests);
+			tests.each{|test|
+				queue(test);
+			}
+		end
 	end
 
 	def run_test(test)
 		reset;
-		@status.msg("Running test #{test}");
+
 		begin
+			test.run_start;
+			@status.msg("Running test #{test} (#{@test_queue.length})");
 			test.run;
-			@successes+=1;
+			sleep 1;
 		rescue Exception => e
-			@failures+=1;
 			@status.msg("Test #{test} \e[31mFAILED: #{e}\e[m");
 			sleep 2;
 		end
+
+		reset;
 	end
 
 	def initialize
@@ -96,12 +234,8 @@ class Suite
 
 		UI.view=@view=View.new(y:1);
 
-		@status=Suite::Status.new(@view.w);
-
-		@current=0;
-		@failures=0;
-		@successes=0;
-		@total=0;
+		@progress=Suite::Progress.new();
+		@status=Suite::Status.new(@progress);
 
 		@view.on('resize',->{
 			@status.redraw;
@@ -111,27 +245,22 @@ class Suite
 		Thread.new{
 			# Wait initially for a moment
 			@status.msg("Initializing...");
-			sleep 0.5;
+			sleep 0.2;
 			loop{
 				if @test_queue.length>0
-					@current+=1;
 					run_test(@test_queue.shift);
-					sleep 0.5;
 				else
-					@status.final(@successes,@failures,@total);
+					@status.final(@progress);
 					#@status.msg("Waiting for tests...");
 					sleep 2;
 				end
-				sleep 0.5;
+				sleep 0.1;
 			}
 		}
 	end
 end
 
 suite=Suite.new;
-
-Dir[__dir__+'/suite/*'].each{|file|
-	suite.test(file);
-};
+suite.queue_directory(__dir__<<'/suite/*');
 
 sleep; # Keep main thread alive
