f12d21481c71a9c16a610846675e94395e90a759
ilmikko <mikko@5x.fi>
2018-02-18 11:48:11 +0000

    Events can now be removed from elements

--- lib/tool/evented.rb
+++ lib/tool/evented.rb
@@ -1,16 +1,21 @@
 module Tool
 	module Evented
 		def fire(name)
-			$console.log("EVT FIRE: #{self}@#{name}");
-			return if @events.nil?
+			return if @events.nil?;
 			return if !@events.key?(name);
-			@events[name].each{|f| f.(); }
+			@events[name].each{ |f| f.() if !f.nil?; };
 		end
 		def on(name,callback)
-			$console.debug("EVT: #{self}@#{name} -> #{callback}");
 			@events={} if @events.nil?;
 			@events[name]=[] if @events[name].nil?;
-			@events[name].push(callback);
+			@events[name] << callback;
+			return [name,callback];
+		end
+		def off(id)
+			name,callback=id;
+			return if @events.nil?;
+			return if !@events.key?(name);
+			@events[name].delete(callback);
 		end
 	end
 end
