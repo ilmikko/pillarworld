e8f1934bb2295638019ff443e2bf5528e8b0f005
ilmikko <mikko@5x.fi>
2018-04-24 01:14:08 +0100

    Updated UI::Wrap to wrap text elements

--- lib/ui/wrap.rb
+++ lib/ui/wrap.rb
@@ -1,11 +1,104 @@
 #
-# UI Wrap tells its children to use the least amount of space possible, effectively "wrapping" their children. However, I'm not sure if this should be something that every element has
+# UI::Wrap wraps the elements across multiple stacks when the stacks are full.
+# This is something I haven't quite figure out yet, but it's basically text wrap with other elements.
+# It's basically a stack of stacks.
+# TODO: This sucks
+# NOTE: This is optimized for text first, I need to create a general algorithm for other stuff
 #
-class UI::Wrap < UI::Border
+# NOTE: This is an experimental version that uses a stack of stacks.
+#class UI::Wrap < UI::Array
+#	def append(*nodes)
+#		super(*nodes);
+#
+#		while nodes.size > 0
+#			node = nodes.shift;
+#
+#			# TODO: If last stack is full, create a new stack
+#			new_stack if @stacks.last.full?;
+#
+#			# Append the nodes to the last stack
+#			@stacks.last.append(node);
+#		end
+#
+#		self
+#	end
+#
+#	def change
+#		# Update our current stack situation
+#		@stacks.each{ |stack| 
+#			stack.change_wh=@wh;
+#			stack.change_xy=@xy;
+#			stack.change;
+#		};
+#
+#		self
+#	end
+#
+#	#######
+#	private
+#	#######
+#	
+#	def new_stack
+#		stack = UI::Stack.new(direction: :row);
+#		stack.own(self);
+#
+#		@stacks << stack;
+#	end
+#
+#	def initialize
+#		super;
+#
+#		@stacks=[];
+#		new_stack;
+#	end
+#end
+class UI::Wrap < UI::Array
 	def change
-		@children.first.change_wh=@wh;
-		@children.first.change_xy=[@xy[0]+@padding,@xy[1]+@padding];
-		@children.first.change;
-		@wh=[@children.first.w+@padding*2,@children.first.h+@padding*2];
+		# Greedy algorithm to stack the lines most optimally
+		# TODO: This doesn't have to re-run every time we resize.
+
+		# TODO: Directions
+		w,h=@wh;
+		x,y=@xy;
+
+		# Normally space in a line
+		primary_space = w;
+
+		# Normally space for lines of text
+		secondary_space = h;
+
+		@children.each{ |c|
+			$console.log("Wrap: #{primary_space},#{secondary_space} vs #{[w,h]}");
+
+			cw = c.content_width;
+
+			if primary_space-cw <= 0
+				# Ran out of primary space, use secondary space
+				primary_space = w;
+
+				secondary_space -= 1; # FIXME: This is wrong, it should be the maximum height
+
+				# Ran out of secondary space, it's over
+				break if secondary_space <= 0;
+			end
+
+			c.change_xy=[x+w-primary_space,y+h-secondary_space];
+			c.change_wh=[primary_space,secondary_space];
+
+			c.change;
+
+			# TODO: Not just cw
+			primary_space -= cw;
+		}
+
+		self
+	end
+
+	#######
+	private
+	#######
+
+	def initialize
+		super;
 	end
 end
--- test/test/50-ui/31-wordwrap.rb
+++ test/test/50-ui/31-wordwrap.rb
@@ -3,18 +3,11 @@
 #
 
 UI.new.show(
-        UI::Padding.new(padding: 8).append(
-                UI::Border.new().append(
-                        UI::Stack.new(direction: :col).append(
-                                UI::TextLine.new("This is a single line of text."),
-                                UI::TextLine.new("Here's another, which is hopefully a bit more sophisticated."),
-                                UI::TextLine.new("This one line of text should be extremely long so that it will wrap correctly on whatever screen resolution you could possibly have. Isn't that great? However, if you have zoomed the terminal screen out too much, the line still won't wrap. That's something you need to fix though, not me. I'm not sure what else I could write about; the content on this page doesn't really matter so my words can be as empty as a fish barrel with no fish. I could have also used the (in)famous lorem ipsum, but let's be honest here; who wants to read a lorem ipsum when you can have a proper conversation with the reader like this? Ain't that right, ain't it? Yeah it is.")
-                        )
-                        #UIParagraph.new("This is a text.\nThis should be on a new line.")
-                        #gets translated to
-                        #UITextLine.new("This is a text."),UITextLine.new("This should be on a new line.")
-                        #gets translated to
-                        #UIText.new("This"),UIText.new("is"),...
-                )
-        )
+	UI::Padding.new(padding: 8).append(
+		UI::Border.new().append(
+			UI::Wrap.new().append(
+				*"Hello, this is a text line wrapping test. The text should be long enough to be wrapping to multiple lines including screens that are of decent size and also console screens where it's usual. I have no idea what I'm writing here but it does it's job.".split(/( )/).map{|s|UI::Text.new(s)}
+			)
+		)
+	)
 );
