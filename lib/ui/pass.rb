5ee7fa05b89ce9f8c8f93f2949e2b3ba0aef8d60
ilmikko <mikko@5x.fi>
2018-06-03 17:30:41 +0100

    Fix UI Pass content size

--- lib/ui/pass.rb
+++ lib/ui/pass.rb
@@ -7,6 +7,14 @@ class UI::Pass < UI::Array
 	# Pass prevents more than 1 children to exist simultaneously.
 	# This is to prevent headaches with overlapping elements (Pass doesn't know how to position them!)
 	# You should have probably used a Flex instead.
+	def content_width
+		@children.first.content_width;
+	end
+
+	def content_height
+		@children.first.content_height;
+	end
+
 	def append(*items)
 		raise "Error: #{self.class} cannot contain multiple elements (tried to have #{items.length+@children.length})." if items.length+@children.length>1;
 		super(*items);
