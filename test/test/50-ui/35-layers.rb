2525d0a2f19caa1419c5f42940b35a6621799a65
ilmikko <mikko@5x.fi>
2018-06-03 17:30:27 +0100

    Layer test works

--- test/test/50-ui/35-layers.rb
+++ test/test/50-ui/35-layers.rb
@@ -2,6 +2,18 @@
 # Multiple layers
 #
 
+# Hack to get a wrapping effect on elements
+# TODO: Silly idea, but would this be something that UI::Layer has?
+# TODO: i.e. Only when specifically requested the contents would fill to max
+# TODO: additionally this could be something of UI::Align, as the problems
+# TODO: only arise when using UI::Align
+class UI::Hack < UI::Layer
+	def change
+		@wh=content_size;
+		super;
+	end
+end
+
 UI.new.show(
 	UI::Layer.new.append(
 		UI::Align.new(horizontalalign: :left, verticalalign: :top).append(
@@ -11,7 +23,11 @@ UI.new.show(
 			UI::Text.new("This is a text in the bottom right corner.")
 		),
 		UI::Align.new.append(
-			UI::Text.new("This is a text.")
+			UI::Hack.new.append(
+				UI::Border.new.append(
+					UI::Text.new("This is a text.")
+				)
+			)
 		),
 	)
 );
